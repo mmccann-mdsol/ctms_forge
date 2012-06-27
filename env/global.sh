489545b1d48bad7960c20debdbe094d5f7408937
Martin McCann <mmccann@mdsol.com>
2012-06-27 12:07:46 +0100

    Changed default CATALINA_BASE value.

--- env/global.sh
+++ env/global.sh
@@ -17,5 +17,7 @@ seed_data_file=$project_dir/DBSchema/Source/seed_data.sql
 
 # --- Tomcat settings 
 CATALINA_HOME=/usr/share/tomcat7/
-CATALINA_BASE=$forge/ctms_tc_base
+CATALINA_BASE=/var/lib/tomcat7 
+
+# --- Document base 
 ctms_doc_base=$project_dir/app
