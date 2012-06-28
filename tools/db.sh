771128190ff50c3ad1d5b156ad74e8280d87fcd6
Ashley Anderson <aanderson@mdsol.com>
2012-06-28 11:44:53 +0100

    Add functions to start/stop MySQL

--- tools/db.sh
+++ tools/db.sh
@@ -1,61 +1,69 @@
 # ---------------------------------------------------------------------------
-# db.sh, Database tools 
-# CTMS database tools 
+# db.sh, Database tools
+# CTMS database tools
 # ---------------------------------------------------------------------------
-# Provides a set of functions to interact directly with the CTMS database. 
+# Provides a set of functions to interact directly with the CTMS database.
 #
-# WARNING: These tools are intended as development aids and should not be 
-#          used in a production environment. 
+# WARNING: These tools are intended as development aids and should not be
+#          used in a production environment.
 # ---------------------------------------------------------------------------
 
-# --- create the database set in the environment --- 
+# --- start mysqld
+startDatabase() {
+	echo "Starting MySQL daemon"
+	$mysqld_cmd &> /dev/null &
+	mysqld_pid=$!
+}
+
+# --- stop mysqld
+stopDatabase() {
+	echo "Stopping MySQL daemon"
+	kill $mysqld_pid
+}
+
+# --- create the database set in the environment ---
 createDatabase() {
 
   echo "Creating Database $ctms_db_name:"
-  
-  $mysql_cmd -uroot -p$mysql_root_pass -e "create database $ctms_db_name"; 
-  $mysql_cmd -uroot -p$mysql_root_pass -e "grant ALL on ${ctms_db_name}.* to '$ctms_db_user' identified by '$ctms_db_pass'" 
-    
+
+  $mysql_cmd -uroot -p$mysql_root_pass -e "create database $ctms_db_name";
+  $mysql_cmd -uroot -p$mysql_root_pass -e "grant ALL on ${ctms_db_name}.* to '$ctms_db_user' identified by '$ctms_db_pass'"
+
 }
 
-# --- drop a database 
+# --- drop a database
 dropDatabase() {
- 
+
   echo "Dropping Database $ctms_db_name:"
-  $mysql_cmd -uroot -p$mysql_root_pass -e "drop database $ctms_db_name"; 
+  $mysql_cmd -uroot -p$mysql_root_pass -e "drop database $ctms_db_name";
 
 }
 
-# --- Load the database schema 
-loadSchema() { 
+# --- Load the database schema
+loadSchema() {
 
-  echo -n 'Loading schema...' 
-  $mysql_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name < $schema_file 
-  echo "done." 
-  
+  echo -n 'Loading schema...'
+  $mysql_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name < $schema_file
+  echo "done."
+
 }
 
-# --- Load the seeddata file 
+# --- Load the seeddata file
 loadSeedData() {
 
-  echo 'Loading seed data...' 
-  $mysql_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name < $seed_data_file 
+  echo 'Loading seed data...'
+  $mysql_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name < $seed_data_file
   echo 'done.'
 }
 
 # --- dump the database to the file specified
-dumpDb() { 
+dumpDb() {
 
-   if [ "$1" == "" ] ; then 
-      echo "You must specify the destination file" 
-      return 
-   fi 
- 
-   $dump_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name > $1 
+  if [ "$1" == "" ] ; then
+    echo "You must specify the destination file"
+    return
+  fi
 
-} 
+  $dump_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name > $1
 
-
-
-
-
+}
