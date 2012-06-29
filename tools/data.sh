4060b8e5cf810188c3aac28f4b467a27c565602a
Martin McCann <mmccann@mdsol.com>
2012-06-29 06:14:29 +0100

    update.

--- tools/data.sh
+++ tools/data.sh
@@ -53,3 +53,45 @@ setUserPass() {
  
 }
 
+# --- Get next db Id 
+getNextDbId() { 
+
+  nextDbId=`$mysql_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name -A -e "update key_gen set current_id=current_id+1; select current_id from key_gen;" | grep '[0-9]'`
+
+}
+
+# --- Create a Study 
+addStudy() {
+  
+  if [ "$1" = "" ] ; then 
+    echo "You must supply a study name" 
+  fi 
+  
+  getNextDbId 
+
+  $mysql_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name -e "insert into drugtrial_def (id, name, status, phase) VALUES ( $nextDbId, '$1', 1, 2 )"; 
+
+}
+
+addStudyAttribute() {
+
+  if [ "$4" = "" ] ; then 
+    echo "You must specify the study, attribute group, attribute, value, and an optional dd_value." 
+    return 
+  fi 
+
+  getNextDbId 
+
+  $mysql_cmd -u$ctms_db_user -p$ctms_db_pass $ctms_db_name -e "insert into attribute_answers (id, attribute_tmpl_id, assoc_obj, assoc_obj_id, name, dd_value) VALUES 
+							      ( $nextDbId, 
+							      (select id from attribute_tmpl where name='$3' and attribute_group_tmpl_id = 
+								(select id from attribute_group_tmpl where name='$2')), 
+							      'drugtrial_def', 
+							      (select id from drugtrial_def where name='$1'), 
+							      '$4', '$5')"; 
+  
+} 
+
+
+
+
