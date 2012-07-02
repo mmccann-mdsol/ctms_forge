f70559ee3c7f6117c558453157a8fbf6b5277e73
Ashley Anderson <aanderson@mdsol.com>
2012-07-02 15:33:05 +0100

    Reduce indentation

--- env/global.sh
+++ env/global.sh
@@ -10,12 +10,12 @@ ctms_doc_base=$project_dir/app
 
 # --- Mysql settings
 if [ "$(uname)" = "Darwin" ] ; then
-    mysqld_cmd=/usr/local/bin/mysqld
-    mysql_cmd=/usr/local/bin/mysql
-    dump_cmd=/usr/local/bin/mysqldump
+  mysqld_cmd=/usr/local/bin/mysqld
+  mysql_cmd=/usr/local/bin/mysql
+  dump_cmd=/usr/local/bin/mysqldump
 else
-    mysqld_cmd=/usr/bin/mysqld
-    mysql_cmd=/usr/bin/mysql
-    dump_cmd=/usr/bin/mysqldump
+  mysqld_cmd=/usr/bin/mysqld
+  mysql_cmd=/usr/bin/mysql
+  dump_cmd=/usr/bin/mysqldump
 fi
 mysql_root_pass=root
