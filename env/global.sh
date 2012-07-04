b8e1edfd87a91b995010556b15b22b593c718eb2
Martin McCann <mmccann@mdsol.com>
2012-07-04 12:38:24 +0100

    Made hostOS a global variable.

--- env/global.sh
+++ env/global.sh
@@ -3,6 +3,10 @@
 # loads environment variables for working on project ctms.
 # ---------------------------------------------------------------------------
 
+
+# --- Check our environment -------------------------------------------------
+hostOS="$(uname)"
+
 # --- Tomcat settings
 if [ -z $CATALINA_HOME ] ; then
   export CATALINA_HOME=/usr/share/tomcat7
@@ -12,16 +16,21 @@ ctms_doc_base=$project_dir/app
 
 # --- Mysql settings
 echo=echo
-if [ "$(uname)" = "Darwin" ] ; then
+if [ "$hostOS" = "Darwin" ] ; then
+
   mysqld_cmd=/usr/local/bin/mysqld
   mysql_cmd=/usr/local/bin/mysql
   dump_cmd=/usr/local/bin/mysqldump
   [ -e "/usr/local/Cellar/coreutils/8.17/libexec/gnubin/echo" ] && echo=gecho
+
 else
+
   mysqld_cmd=/usr/bin/mysqld
   mysql_cmd=/usr/bin/mysql
   dump_cmd=/usr/bin/mysqldump
+
 fi
+
 mysql_root_pass=root
 
 # --- Top level help function
